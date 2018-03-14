9e85b96e02c97c47ef7ba9612cc21045119beea5
Jeff Heldridge <bull5150@hotmail.com>
2018-03-13 22:49:51 -0500

    Initial build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GooglemapComponent } from './googlemap/googlemap.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GooglemapComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/googlemap/googlemap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-googlemap',
+  templateUrl: './googlemap.component.html',
+  styleUrls: ['./googlemap.component.css']
+})
+export class GooglemapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
