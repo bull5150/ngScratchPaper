43b90c0cad060b3586299fccd7bb934a7fd562dc
Heldridge, Jeffrey M <Jeffrey.Heldridge@tdameritrade.com>
2018-03-19 16:24:25 -0500

    Forms Add

---
+++ src/app/formsreview/formsreview.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { form_data } from '../models/form_data';
+import {NgbDateStruct} from '@';
+
+@Component({
+  selector: 'forms',
+  templateUrl: './formsreview.component.html',
+  styleUrls: ['./formsreview.component.css']
+})
+export class FormsreviewComponent implements OnInit {
+
+  public formData: form_data[];
+  public model: Date;
+
+  constructor() { 
+  
+  }
+
+  ngOnInit() {
+    this.formData = [{
+      fname: null,
+      lname: null,
+      email: null,
+      phone: null,
+      cont_method: null,
+      gender: null
+    }]
+  }
+
+  dataFill(){
+    this.formData = [{
+      fname: "Frankie",
+      lname: "Muniz",
+      email: "frankthetank@muniz.com",
+      phone: "402-444-4444",
+      cont_method: "He drives race cars apparently",
+      gender: "female"
+    }]
+  }
+
+  dataClear(){
+    console.log(this.formData[0].gender)
+    this.formData = [{
+      fname: null,
+      lname: null,
+      email: null,
+      phone: null,
+      cont_method: null,
+      gender: null
+    }]
+  }
+}
